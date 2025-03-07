666c97a0f8a4762c9d4f87149882fc14c6fbaff8
Anrijs Vitolins <anrijs.vitolins@ebit.no>
2025-03-07 16:14:15 +0200

    Test fix (differences Windows and Linux)

--- Source/Salix.AspNetCore.TitlePage.Tests/DateTimeExtensionsTests.cs
+++ Source/Salix.AspNetCore.TitlePage.Tests/DateTimeExtensionsTests.cs
@@ -48,8 +48,8 @@ namespace Salix.AspNetCore.TitlePage.Tests
             _output.WriteLine("    Days: " + textDays.ToString());
             _output.WriteLine("Expected: " + expected);
             var result = startTime.ToHumanDateString(endTime, textDays);
-            _output.WriteLine("  Actual: " + expected);
-            result.Should().Be(expected);
+            _output.WriteLine("  Actual: " + result);
+            result.Should().StartWith(expected);
         }
 
         public static IEnumerable<object[]> HumanDateStringTestData()
@@ -60,11 +60,11 @@ namespace Salix.AspNetCore.TitlePage.Tests
             yield return new object[] { new DateTime(2016, 2, 1), new DateTime(2016, 2, 1), 5, "February 1, 2016" };
             yield return new object[] { new DateTime(2016, 2, 1), new DateTime(2016, 2, 2), 5, "1. – 2. February, 2016" };
             yield return new object[] { new DateTime(2016, 3, 31), new DateTime(2016, 4, 1), 5, "March 31 – April 1, 2016" };
-            yield return new object[] { new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 14, 23, 15), null, 5, "Today (2:23 PM)" };
+            yield return new object[] { new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 14, 23, 15), null, 5, "Today (2:23" };
             yield return new object[] { DateTime.Now.AddDays(1), null, 5, "Tomorrow" };
             yield return new object[] { DateTime.Now.AddDays(2), null, 5, "The day after tomorrow" };
             yield return new object[] { DateTime.Now.AddDays(3), null, 5, "Three days from now" };
-            yield return new object[] { new DateTime(DateTime.Now.AddDays(-1).Year, DateTime.Now.AddDays(-1).Month, DateTime.Now.AddDays(-1).Day, 14, 23, 15), null, 5, "Yesterday (2:23 PM)" };
+            yield return new object[] { new DateTime(DateTime.Now.AddDays(-1).Year, DateTime.Now.AddDays(-1).Month, DateTime.Now.AddDays(-1).Day, 14, 23, 15), null, 5, "Yesterday (2:23" };
             yield return new object[] { DateTime.Now.AddDays(-2), null, 5, "The day before yesterday" };
             yield return new object[] { DateTime.Now.AddDays(-3), null, 5, "Three days ago" };
             yield return new object[] { DateTime.Now.AddDays(4), null, 5, "4 days from now" };
@@ -77,7 +77,7 @@ namespace Salix.AspNetCore.TitlePage.Tests
             yield return new object[] { DateTime.Now.AddDays(-3), null, 2, DateTime.Now.AddDays(-3).ToString("M") };
             yield return new object[] { DateTime.Now.AddDays(-2), null, 2, "The day before yesterday" };
             yield return new object[] { DateTime.Now.AddDays(-2), null, 1, DateTime.Now.AddDays(-2).ToString("M") };
-            yield return new object[] { new DateTime(DateTime.Now.AddDays(-1).Year, DateTime.Now.AddDays(-1).Month, DateTime.Now.AddDays(-1).Day, 14, 23, 15), null, 1, "Yesterday (2:23 PM)" };
+            yield return new object[] { new DateTime(DateTime.Now.AddDays(-1).Year, DateTime.Now.AddDays(-1).Month, DateTime.Now.AddDays(-1).Day, 14, 23, 15), null, 1, "Yesterday (2:23" };
             yield return new object[] { DateTime.Now.AddDays(-1), null, 0, DateTime.Now.AddDays(-1).ToString("M") };
         }
     }
